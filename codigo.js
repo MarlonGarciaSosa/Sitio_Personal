4cee7b919f79e357e9c8fa06fc63138da597b0e7
Marlon <155335960+MarlonGarciaSosa@users.noreply.github.com>
2024-02-07 19:56:52 -0500

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,162 @@
+function grupo(){
+    const clase = document.querySelector(".header-right__p");
+    clase.classList.remove(".negrita");
+    const grupo = document.getElementById("grupo");
+    grupo.classList.add("negrita");
+
+    //eliminar negrita
+    const quién_soy = document.getElementById("quién_soy");
+    const programo = document.getElementById("programo");
+    const diseño = document.getElementById("diseño");
+    const contacto = document.getElementById("contacto");
+    quién_soy.classList.remove("negrita");
+    programo.classList.remove("negrita");
+    diseño.classList.remove("negrita");
+    contacto.classList.remove("negrita");
+
+    //eliminar main
+    const grupo__main = document.getElementById("grupo__main");
+    const quién_soy__main = document.getElementById("quién_soy__main");
+    const programo__main = document.getElementById("programo__main");
+    const diseño__main = document.getElementById("diseño__main");
+    const contacto__main = document.getElementById("contacto__main");
+    grupo__main.classList.remove("display_none");
+    quién_soy__main.classList.add("display_none");
+    programo__main.classList.add("display_none");
+    diseño__main.classList.add("display_none");
+    contacto__main.classList.add("display_none");
+
+    
+}
+
+
+
+
+function quién_soy() {
+    const clase = document.querySelector(".header-right__p");
+    clase.classList.remove("negrita");
+    const quién_soy = document.getElementById("quién_soy");
+    quién_soy.classList.add("negrita");
+
+    //eliminar negrita
+    const grupo = document.getElementById("grupo");
+    const programo = document.getElementById("programo");
+    const diseño = document.getElementById("diseño");
+    const contacto = document.getElementById("contacto");
+    grupo.classList.remove("negrita");
+    programo.classList.remove("negrita");
+    diseño.classList.remove("negrita");
+    contacto.classList.remove("negrita");
+
+    //eliminar main
+    const grupo__main = document.getElementById("grupo__main");
+    const quién_soy__main = document.getElementById("quién_soy__main");
+    const programo__main = document.getElementById("programo__main");
+    const diseño__main = document.getElementById("diseño__main");
+    const contacto__main = document.getElementById("contacto__main");
+    grupo__main.classList.add("display_none");
+    quién_soy__main.classList.remove("display_none");
+    programo__main.classList.add("display_none");
+    diseño__main.classList.add("display_none");
+    contacto__main.classList.add("display_none");
+
+}
+
+
+
+
+function programo() {
+    const clase = document.querySelector(".header-right__p");
+    clase.classList.remove("negrita");
+    const programo = document.getElementById("programo");
+    programo.classList.add("negrita");
+
+    //eliminar negrita
+    const grupo = document.getElementById("grupo");
+    const quién_soy = document.getElementById("quién_soy");
+    const diseño = document.getElementById("diseño");
+    const contacto = document.getElementById("contacto");
+    grupo.classList.remove("negrita");
+    quién_soy.classList.remove("negrita");
+    diseño.classList.remove("negrita");
+    contacto.classList.remove("negrita");
+
+    //eliminar main
+    const grupo__main = document.getElementById("grupo__main");
+    const quién_soy__main = document.getElementById("quién_soy__main");
+    const programo__main = document.getElementById("programo__main");
+    const diseño__main = document.getElementById("diseño__main");
+    const contacto__main = document.getElementById("contacto__main");
+    grupo__main.classList.add("display_none");
+    quién_soy__main.classList.add("display_none");
+    programo__main.classList.remove("display_none");
+    diseño__main.classList.add("display_none");
+    contacto__main.classList.add("display_none");
+}
+
+
+
+
+
+function diseño() {
+    const clase = document.querySelector(".header-right__p");
+    clase.classList.remove("negrita");
+    const diseño = document.getElementById("diseño");
+    diseño.classList.add("negrita");
+
+    //eliminar negrita
+    const grupo = document.getElementById("grupo");
+    const quién_soy = document.getElementById("quién_soy");
+    const programo = document.getElementById("programo");
+    const contacto = document.getElementById("contacto");
+    grupo.classList.remove("negrita");
+    quién_soy.classList.remove("negrita");
+    programo.classList.remove("negrita");
+    contacto.classList.remove("negrita");
+
+    //eliminar main
+    const grupo__main = document.getElementById("grupo__main");
+    const quién_soy__main = document.getElementById("quién_soy__main");
+    const programo__main = document.getElementById("programo__main");
+    const diseño__main = document.getElementById("diseño__main");
+    const contacto__main = document.getElementById("contacto__main");
+    grupo__main.classList.add("display_none");
+    quién_soy__main.classList.add("display_none");
+    programo__main.classList.add("display_none");
+    diseño__main.classList.remove("display_none");
+    contacto__main.classList.add("display_none");
+}
+
+
+
+
+
+function contacto() {
+    const clase = document.querySelector(".header-right__p");
+    clase.classList.remove("negrita");
+    const contacto = document.getElementById("contacto");
+    contacto.classList.add("negrita");
+
+    //eliminar negrita
+    const grupo = document.getElementById("grupo");
+    const quién_soy = document.getElementById("quién_soy");
+    const programo = document.getElementById("programo");
+    const diseño = document.getElementById("diseño");
+    grupo.classList.remove("negrita");
+    quién_soy.classList.remove("negrita");
+    programo.classList.remove("negrita");
+    diseño.classList.remove("negrita");
+
+    //eliminar main
+    const grupo__main = document.getElementById("grupo__main");
+    const quién_soy__main = document.getElementById("quién_soy__main");
+    const programo__main = document.getElementById("programo__main");
+    const diseño__main = document.getElementById("diseño__main");
+    const contacto__main = document.getElementById("contacto__main");
+    grupo__main.classList.add("display_none");
+    quién_soy__main.classList.add("display_none");
+    programo__main.classList.add("display_none");
+    diseño__main.classList.add("display_none");
+    contacto__main.classList.remove("display_none");
+
+}
